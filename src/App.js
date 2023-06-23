c24e17900bbf7668dc88943214da730230de6dea
Kihyun Kim <rlgus3689@gmail.com>
2023-06-24 01:36:38 +1200

    create ExpenseItem and import it into App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is visible as well!!</p>
+      <ExpenseItem></ExpenseItem>;
     </div>
   );
 }
