9e8fdcf037e387d116e51f086c41523d6df569be
Kihyun Kim <rlgus3689@gmail.com>
2023-06-24 01:59:54 +1200

    modify JSX in ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>March 28th 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$300</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
