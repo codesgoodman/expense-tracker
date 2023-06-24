d27137cc99ed64dd92a19914318fffbaae1ddb29
Kihyun Kim <rlgus3689@gmail.com>
2023-06-24 17:01:18 +1200

    add variables and replace harcoded values in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const ExpenseDate = new Date(2023, 6, 24);
+  const ExpenseTitle = "Car insurance";
+  const ExpenseAmount = 300;
   return (
     <div className="expense-item">
-      <div>March 28th 2023</div>
+      <div>{ExpenseDate.toDateString()}</div>
       <div>
-        <h2 className="expense-item__description">Car Insurance</h2>
-        <div className="expense-item__price">$300</div>
+        <h2 className="expense-item__description">{ExpenseTitle}</h2>
+        <div className="expense-item__price">${ExpenseAmount}</div>
       </div>
     </div>
   );
