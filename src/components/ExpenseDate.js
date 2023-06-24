5097b422584ccb47d120effd5b61d64ed1f535ec
Kihyun Kim <rlgus3689@gmail.com>
2023-06-24 20:55:18 +1200

    change order of year and day

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -7,8 +7,8 @@ function ExpenseDate(props) {
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
-      <div className="expense-date__day">{day}</div>
       <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day ">{day}</div>
     </div>
   );
 }
