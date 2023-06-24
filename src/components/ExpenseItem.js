23ba057da7c2457581e3f6e81db76a6983e08836
Kihyun Kim <rlgus3689@gmail.com>
2023-06-24 20:07:31 +1200

    refactor dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div>
         <h2 className="expense-item__description">{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
