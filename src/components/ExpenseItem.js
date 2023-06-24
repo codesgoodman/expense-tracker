1fca643a8ca171c9bb4635efc3f3feb6d8de056e
Kihyun Kim <rlgus3689@gmail.com>
2023-06-24 18:55:27 +1200

    call prop attributes from app in ExenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const ExpenseDate = new Date(2023, 6, 24);
-  const ExpenseTitle = "Car insurance";
-  const ExpenseAmount = 300;
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{ExpenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div>
-        <h2 className="expense-item__description">{ExpenseTitle}</h2>
-        <div className="expense-item__price">${ExpenseAmount}</div>
+        <h2 className="expense-item__description">{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
