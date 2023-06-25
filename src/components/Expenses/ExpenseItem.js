0ca841c38ae7524a4d2d904c5c90134123f2432b
Kihyun Kim <rlgus3689@gmail.com>
2023-06-26 01:48:33 +1200

    add temp button in ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -11,6 +11,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button>Change Title</button>
     </Card>
   );
 }
