e398b4311fe42d43823e0a60154998147db41c93
Kihyun Kim <rlgus3689@gmail.com>
2023-06-26 17:37:00 +1200

    update newExpense css

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -13,6 +13,13 @@ const ExpenseForm = () => {
           <label>Amount</label>
           <input type="number" min="0.01" step="0.01" />
         </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2022-01-01" max="2024-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
