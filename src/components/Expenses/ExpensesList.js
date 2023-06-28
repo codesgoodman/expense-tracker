b562bc89648dd70598a9c00d5600aec1e4149e0b
Kihyun Kim <rlgus3689@gmail.com>
2023-06-28 16:03:10 +1200

    refactor code

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -3,22 +3,19 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
-  let expensesContent = <p>No expense found</p>;
-  if (props.item.length === 0) {
+  if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses</h2>;
   }
   return (
     <ul className="expenses-list">
-      {
-        (expensesContent = props.item.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        )))
-      }
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </ul>
   );
 };
